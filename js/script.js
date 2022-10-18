72d997c4768d3745f1cd4f4d580535077835ff08
sder12 <sara.cetto@gmail.com>
2022-10-18 11:50:02 +0200

    Add startgame function and ui function

--- js/script.js
+++ js/script.js
@@ -1,28 +1,64 @@
 const btnPlay = document.getElementById("button-play");
 const mainDiv = document.querySelector("main");
 
-btnPlay.addEventListener("click", function () {
+btnPlay.addEventListener("click", startgame);
+
+//FUNCTION-------------------------------------------
+/** STARTGAME
+ * Description With the start/click create Grid - Square
+ */
+function startgame() {
   mainDiv.innerHTML = "";
   //Add grid
-  const gridDiv = document.createElement("div");
-  gridDiv.classList.add("grid");
+  const gridDiv = createGridDiv();
   mainDiv.append(gridDiv);
 
   // Create and add square
   for (let i = 1; i < 101; i++) {
-    const squareDiv = document.createElement("div");
-    squareDiv.classList.add("square");
-    squareDiv.classList.add("square-simple");
-    squareDiv.innerHTML = i;
+    const squareDiv = createSquareDiv(i);
     // click add blue
     squareDiv.addEventListener("click", clickedSquare);
     gridDiv.append(squareDiv);
   }
+}
 
-});
+// UI FUNCTION -- create elements in html
+/** GRID
+ * Description Create a div with class grid
+ * @returns {object}
+ */
+function createGridDiv() {
+  const divGrid = document.createElement("div");
+  divGrid.classList.add("grid");
+  return divGrid;
+}
+/** SQUARE
+ * Description Create a div with two classes and a number inside
+ * @param {number} numberInside
+ * @returns {object}
+ */
+function createSquareDiv(numberInside) {
+  const divSquare = document.createElement("div");
+  divSquare.classList.add("square");
+  divSquare.classList.add("square-simple");
+  divSquare.innerHTML = numberInside;
+  return divSquare;
+}
 
-//FUNCTION-------------------------------------------
+/**FUNZIONE VOID > non ha return: CLICK del singolo NUMERO
+ * Quando elemento è cliccato aggiungo la classe del colore a quell'elemento
+ * e poi lo visualizzo nella console
+ */
+function clickedSquare() {
+  this.classList.add("blue");
+  const clickedNumber = this.textContent;
+  console.log(clickedNumber);
+}
 
+
+
+
+//FUNCTION RANDOM NUMBER-----------------------
 //RANDOM NUMBERS GENERATOR W3school
 function getRndInteger(min, max) {
   return Math.floor(Math.random() * (max - min + 1)) + min;
@@ -44,15 +80,4 @@ function generateArrayRandomNumbers(arrayLength) {
     }
   }
   return arrayNumbers;
-}
-
-
-/**FUNZIONE VOID > non ha return: CLICK del singolo NUMERO
- * Quando elemento è cliccato aggiungo la classe del colore a quell'elemento
- * e poi lo visualizzo nella console
- */
-function clickedSquare() {
-  this.classList.add("blue");
-  const clickedNumber = this.textContent;
-  console.log(clickedNumber);
 }
